f8e9f4beb3cafebb9cadfbb30291e220bcb71ffb
Bozhidar Batsov <lordbad@gmail.com>
2010-11-04 18:21:42 +0200

    added a placeholder solution

---
+++ src/main/java/jia/begins/examples/basics/bozhidar/batsov/Exercise1.java
@@ -0,0 +1,9 @@
+package jia.begins.examples.basics.bozhidar.batsov;
+
+/**
+ * @author <a href="mailto:bozhidar@drow.bg">Bozhidar Batsov</a>
+ */
+public class Exercise1 {
+    public static void main(String[] args) {
+    }
+}
